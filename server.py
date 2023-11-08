2a28cc2e87418b9446d22abdcc408ecc06e8e621
FranciscoF <129874231+Frankcisco41@users.noreply.github.com>
2023-11-08 20:03:06 -0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+# 1.- Crear ruta y función para renderizar plantilla formulario
+
+# 2.- Crear ruta y función para procesar datos
+
+if __name__ == '__main__':
+    app.run(debug=True)
